b8804113b033d533004bb6f9524daab7dd5e2ab9
Ricter1229 <a095280403988@gmail.com>
2024-12-13 05:45:17 +0800

    合併 store detail 暫時成功

--- src/router/router.js
+++ src/router/router.js
@@ -11,28 +11,21 @@ import ResetPassword from "@/views/secure/ResetPassword.vue";
 
 // movie
 import Movie from "@/views/pages/Movie.vue";
-import MovieFront from "@/views/pages/MovieFront.vue";
-import OneMovie from "@/views/pages/OneMovie.vue";
 
 // store
 import CinemaBack from "../views/cinemaback/CinemaBack.vue";
 import CinemaModal from "../views/cinemaback/CinemaModal.vue";
-import CinemaMovieSchedule from "../views/cinemaback/CinemaMovieSchedule.vue";
 import CinemaAdsSet from "@/views/cinemaback/CinemaAdsSet.vue";
 
 // booking
 import InsertSeatingList from "@/views/booking/InsertSeatingList.vue";
 
 // temp
-import TempMoney from "@/views/TempMoney.vue"
-import TheWelcome from '../components/TheWelcome.vue'
 import MovieCalender from "@/views/movie/MovieCalender.vue";
 import MovieWithStore from "@/views/movie/MovieWithStore.vue";
 
 const routes = [
 // temp
-    { path: "/money", name: "temp-money-link", component: TempMoney },
-    { path: "/test", name: "test-link", component: TheWelcome },
     { path: "/testCalender", name: "test-calender-link", component: MovieCalender },
     { path: "/testMovieWithStore", name: "test-testMovieWithStore-link", component: MovieWithStore },
 
@@ -46,28 +39,25 @@ const routes = [
 
 // movie
     { name:"movie-link" , path:"/movie" , component: Movie},
-    { name:"moviefront-link" , path:"/moviefront" , component: MovieFront},
-    { name:"onemovie-link" , path:"/onemovie/:id" , component: OneMovie ,props: true},
 
 // store
 
     {name:"back-link",path:'/backend',component:CinemaBack},
-    {name:"schedule-link",path:'/schedule',component:CinemaMovieSchedule},
     {name:"ads-set-link",path:'/ads/set',component:CinemaAdsSet},
 
 // booking
     { path: "/", name: "main-page-link", component: MainPage },
     // { path: "/movie", name: "movie-link", component: () => import('@/views/booking/choose_store_and_time/JustTempForBooking.vue') },
-    { 
-        path: "/booking", name: "booking-link", component: InsertSeatingList,
-        children: [
-            {
-                path: "choose-seat",
-                name: "choose-seat-link",
-                component: () => import('@/views/booking/ChooseSeat.vue'), 
-            },
-        ]
-    },
+    // { 
+    //     path: "/booking", name: "booking-link", component: InsertSeatingList,
+    //     children: [
+    //         {
+    //             path: "choose-seat",
+    //             name: "choose-seat-link",
+    //             component: () => import('@/views/booking/ChooseSeat.vue'), 
+    //         },
+    //     ]
+    // },
 ]
 
 export default createRouter({
